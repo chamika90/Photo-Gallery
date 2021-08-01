0698b5f85bdafa6f40caf2220e65265435959866
chamika <chamika.hasanthi@axiatadigitallabs.com>
2021-08-02 02:00:57 +0530

    Configure Redux store

--- App.js
+++ App.js
@@ -4,18 +4,51 @@
  */
 
 import React, {useEffect} from 'react';
-
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import {persistStore} from 'redux-persist';
 import SplashScreen from 'react-native-splash-screen';
+
 import AppNavigator from './src/navigation';
+import configureStore from './store/configureStore';
+import {theme} from './src/config/theme';
+
+const {colors} = theme;
+const {store} = configureStore();
 
 const App = () => {
+  const persister = persistStore(store);
+
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
     }, 2500);
   }, []);
 
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <PersistGate
+        loading={
+          <View style={styles.loaderContainer}>
+            <ActivityIndicator
+              size={'large'}
+              color={colors.primaryLoaderColor}
+            />
+          </View>
+        }
+        persistor={persister}>
+        <AppNavigator />
+      </PersistGate>
+    </Provider>
+  );
 };
 
+const styles = StyleSheet.create({
+  loaderContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default App;
---
+++ store/configureStore.js
@@ -0,0 +1,34 @@
+import {createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+import createSagaMiddleware from 'redux-saga';
+import expireReducer from 'redux-persist-expire';
+
+import rootReducers from 'redux/reducers';
+import sagas from 'sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whiteList: ['galleryReducer'],
+  transforms: [
+    expireReducer('galleryReducer', {
+      expireSeconds: 60 * 60 * 24,
+      expiredState: [],
+      autoExpire: true,
+    }),
+  ],
+};
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+
+const persister = persistStore(store);
+const configureStore = () => {
+  return {persister, store};
+};
+sagaMiddleware.run(sagas);
+
+export default configureStore;
