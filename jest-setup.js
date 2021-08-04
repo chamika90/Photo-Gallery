75f4ecd7f1059ff4913181d948e8eea3d98c9a39
chamika <chamika.hasanthi@axiatadigitallabs.com>
2021-08-04 20:10:49 +0530

    Mock libraries

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,7 @@
 import 'react-native';
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 
 jest.mock('rn-fetch-blob', () => require('./_mocks_/rn-fetch-blob-mock.js'));
+jest.mock('axios', () => require('./_mocks_/axios-mock.js'));
