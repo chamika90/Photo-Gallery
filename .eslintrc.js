099d93a93d6a258e4702421f52905ba8895e6342
chamika <chamika.hasanthi@axiatadigitallabs.com>
2021-08-16 00:49:02 +0530

    Setup eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+    'react/prop-types': 'off',
+    // indent: ['error', 4],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    semi: ['error', 'always'],
+  },
 };
